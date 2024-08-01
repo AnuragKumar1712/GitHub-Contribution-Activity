0a8557535602b6e1ad77dae264abcf506640690e
Anurag Kumar <134754947+AnuragKumar1712@users.noreply.github.com>
2024-08-02 00:13:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const container = document.getElementById("_container");
+
+let greenColorCodes = [
+    "#7CFC00",
+    "#78AB46",
+    "#3B5323",
+    "#458B00",
+    "#BCED91",
+    "#61B329",
+    "#5DFC0A",
+    "#4CBB17",
+    "#4AC948",
+    "#00EE00",
+    "#33FF33",
+    "#00C957",
+];
+
+const numberOfSquares = 90;
+
+for(let i = 0; i < numberOfSquares; i++){
+
+    const activity = document.createElement("div");
+    activity.classList.add("activity");
+
+    activity.addEventListener("mouseover",()=>{
+        addColor(activity);
+    });
+
+    activity.addEventListener("mouseout",()=>{
+        stickColor(activity);
+    });
+
+    container.appendChild(activity);
+}
+
+function addColor(squaresTile){
+    const gettingColor = greenColors();
+    squaresTile.style.backgroundColor = gettingColor;
+    squaresTile.style.boxshadow = ` 0 0 2px ${gettingColor}, 0 0 10px ${gettingColor}`;
+}
+
+function stickColor(squaresTile){
+    const gettingColor = greenColors();
+    squaresTile.style.backgroundColor = gettingColor;
+    squaresTile.style.boxshadow = ` 0 0 2px ${gettingColor}, 0 0 10px ${gettingColor}`;
+}
+
+function greenColors(){
+    return greenColorCodes[Math.floor(Math.random() * greenColorCodes.length)]
+}
